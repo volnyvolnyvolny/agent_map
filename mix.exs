ae14beea5fc5f0f2b690b5739188000de8615280
Valentin <flomop@ya.ru>
2019-03-20 18:19:35 +0300

    Benchee is added.

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule AgentMap.Mixfile do
     [
       {:heap, "~> 2.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:credo, "~> 1.0", only: :dev}
+      {:credo, "~> 1.0", only: :dev},
+      {:benchee, "~> 0.14", only: :test}
     ]
   end
 
